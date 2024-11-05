70e5bebfef60ab0e217e26c0ae5f5a0f65121fed
Mateusz Praski <prask1.m3@gmail.com>
2024-11-05 19:24:33 +0100

    Daily task nr. 2914 leetcode

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Solution3163 solution3163 = new Solution3163();
-        System.out.println(solution3163.compressedString("aaaaaabcd"));
+        Solution2914 solution2914 = new Solution2914();
+        System.out.println(solution2914.minChanges("0000"));
     }
 }
